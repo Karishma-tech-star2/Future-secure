f524b4c854f8b871ec358ce17f3e85c626721c3e
Karishma-tech-star2 <compute.edu.24@gmail.com>
2026-09-25 17:09:45 +0530

    script.js

--- script.js
+++ script.js
@@ -31,6 +31,99 @@ const how_observer = new IntersectionObserver((entries)=>{
 
 how_observer.observe(how_section);
 
+/*Plan cards click*/
+const plan_buttons = document.querySelectorAll(".plan-btn");
+const plan_modal = document.querySelector("#plan-modal");
+const modal_close = document.querySelector(".modal-close");
+
+const modal_plan_name = document.querySelector("#modal-plan-name");
+const modal_plan_description = document.querySelector("#modal-plan-description");
+const modal_plan_amount = document.querySelector("#modal-plan-amount");
+const modal_plan_duration = document.querySelector("#modal-plan-duration");
+const modal_plan_features = document.querySelector("#modal-plan-features");
+
+const plan_data = {
+    starter: {
+        name: "Starter",
+        description: "For small and early goals",
+        amount: "₹1,000",
+        duration: "5+ Years",
+        features: [
+            "Flexible investment planning",
+            "Goal-based planning",
+            "Growth estimation"
+        ]
+    },
+
+    growth: {
+        name: "Growth",
+        description: "For long-term future goals",
+        amount: "₹3,000",
+        duration: "8+ Years",
+        features: [
+            "Long-term goal planning",
+            "SIP growth estimation",
+            "Investment tracking"
+        ]
+    },
+
+    "future-plus": {
+        name: "Future Plus",
+        description: "For bigger future goals",
+        amount: "₹5,000",
+        duration: "10+ Years",
+        features: [
+            "Higher investment planning",
+            "Future goal estimation",
+            "Long-term growth tracking"
+        ]
+    }
+};
+
+
+plan_buttons.forEach((button)=>{
+    button.addEventListener("click",()=>{
+
+        current_plan = button.dataset.plan;
+
+        const selected_data = plan_data[current_plan];
+
+        modal_plan_name.textContent = selected_data.name;
+        modal_plan_description.textContent = selected_data.description;
+        modal_plan_amount.textContent = selected_data.amount;
+        modal_plan_duration.textContent = selected_data.duration;
+
+        modal_plan_features.innerHTML = "";
+
+        selected_data.features.forEach((feature)=>{
+            const li = document.createElement("li");
+            li.textContent = feature;
+            modal_plan_features.appendChild(li);
+        });
+
+        plan_modal.style.display = "flex";
+
+    });
+});
+modal_close.addEventListener("click",()=>{
+    plan_modal.style.display = "none";
+});
+const modal_select_plan =
+    document.querySelector("#modal-select-plan");
+
+modal_select_plan.addEventListener("click",(event)=>{
+
+    event.preventDefault();
+
+    localStorage.setItem(
+        "future_secure_selected_plan",
+        current_plan
+    );
+
+    window.location.href = "signup.html";
+
+});
+
 /*  SIP Calculator Section */
 const monthly_investment = document.querySelector("#monthly-investment");
 const investment_duration = document.querySelector("#investment-duration");
@@ -69,4 +162,36 @@ calculate_btn.addEventListener("click",()=>{
     result_values[1].textContent = `₹${Math.round(estimated_returns).toLocaleString("en-IN")}`;
     result_values[2].textContent = `₹${Math.round(total_value).toLocaleString("en-IN")}`;
     console.log(result_values.length);
+});
+
+/*Menu_Toggle*/
+const menu_toggle = document.querySelector(".menu-toggle");
+const nav = document.querySelector("nav");
+
+menu_toggle.addEventListener("click",()=>{
+    nav.classList.toggle("active");
+});
+
+
+/*FAQ questions*/
+const faq_questions = document.querySelectorAll(".faq-question");
+
+faq_questions.forEach((question)=>{
+    question.addEventListener("click",()=>{
+
+        const answer = question.nextElementSibling;
+        const icon = question.querySelector("i");
+
+        if(answer.style.maxHeight){
+            answer.style.maxHeight = null;
+            icon.classList.remove("fa-minus");
+            icon.classList.add("fa-plus");
+        }
+        else{
+            answer.style.maxHeight = answer.scrollHeight + "px";
+            icon.classList.remove("fa-plus");
+            icon.classList.add("fa-minus");
+        }
+
+    });
 });
